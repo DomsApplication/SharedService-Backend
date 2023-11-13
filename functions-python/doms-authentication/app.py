c32b5943a124701907086a7544338ca3fc3c346b
00004512 <00004512@arisglobal.com>
2023-11-13 19:32:14 +0530

    python based lambda code

---
+++ functions-python/doms-authentication/app.py
@@ -0,0 +1,29 @@
+import boto3
+import sys
+import os
+import json
+import uuid
+from datetime import datetime
+
+def lambda_handler(event, context):
+    print('Request from AUTHENTICATION...')
+    print('OS ::: ' , os.name)
+    print('Python version ::: ' , sys.version)
+    print('Version info ::: ' , sys.version_info)
+
+    print('event :::', event)
+
+    response = {
+        'id': str(uuid.uuid4()),
+        'date': str(datetime.timestamp(datetime.now())),
+        'stage': 'DEV',
+        'message': 'Hello World!...'
+    }
+
+    print(response)
+
+    return {
+        'statusCode': 200,
+        'headers': {},
+        'body': json.dumps(response)
+    }
