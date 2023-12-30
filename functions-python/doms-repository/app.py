236bc5fc57f5f0cc547dc57ac9195dca8609233d
mahesha boraiah <mbmahesha47@gmail.com>
2023-12-30 18:26:01 +0530

    repository lambda code

--- functions-python/doms-repository/app.py
+++ functions-python/doms-repository/app.py
@@ -8,11 +8,12 @@ def lambda_handler(event, context):
         if 'path' not in event or 'httpMethod' not in event:
             return sendResponse(400, {'message' : 'Application service expecting http request.'})
 
+        logInfo('event', event)
         logInfo('Path', event['path'])
         logInfo('httpMethod', event['httpMethod'])
         logInfo('body', event['body'])
 
-        if event['path'] == '/repo/write/entity':
+        if event['path'] == '/api/repo/entity':
 
             requestBody = json.loads(event['body'])
             if 'entity' not in requestBody:
@@ -101,9 +102,23 @@ def lambda_handler(event, context):
             else:
                 msg = {'message' : 'Method: ' + event['httpMethod'] + ' not allowed for the requested path:' + event['path'] }
                 return sendResponse(405, msg)    
+        
+        # Get query the data from dynamodb        
+        elif event['path'].startswith('/api/repo/record/'):
+            ### GET
+            if event['httpMethod'] == 'GET':
+                requestEntityName = event['path'].replace('/api/repo/record/', '')
+                logInfo("app/requestEntityName", requestEntityName)
+
+                return sendResponse(201, {'message' : 'success'})
+            else:
+                msg = {'message' : 'Method: ' + event['httpMethod'] + ' not allowed for the requested path:' + event['path'] }
+                return sendResponse(405, msg)    
+
         else:    
             msg = {'message' : 'Path :' + event['path'] + ' not found.'}
             return sendResponse(404, msg)
+
     except Exception as error:
         logError('Exception in main function', error)
         return sendResponse(500, {'error' : str(error)})
