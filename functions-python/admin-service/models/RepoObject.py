1e1d07818b987d65f16c1aba46290c07143abadd
mahesha boraiah <mbmahesha47@gmail.com>
2024-05-04 16:17:32 +0530

    handled search

--- functions-python/admin-service/models/RepoObject.py
+++ functions-python/admin-service/models/RepoObject.py
@@ -7,7 +7,7 @@ from pydantic import BaseModel, Field, EmailStr, Json
 
 # Define your entity class using Pydantic BaseModel
 class RepoObject(BaseModel):
-    unique_id: str = Field(min_length=4, max_length=20)
+    unique_id: str = Field(min_length=0, max_length=20)
     entity: str = Field(min_length=3, max_length=20)
     version: Optional[int] = Field(gt=0, lt=10, default=1)
     payload: Json
