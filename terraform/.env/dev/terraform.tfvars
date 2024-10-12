2cd061b219a8398426ef1ed94a0d16c74c1c32a8
mahesha boraiah <mbmahesha47@gmail.com>
2024-10-13 01:51:00 +0530

    created core-lib

---
+++ terraform/.env/dev/terraform.tfvars
@@ -0,0 +1,10 @@
+stack_name      = "shavika"
+deploy_env      = "dev"
+product         = "doms"
+product_version = "1.0.0"
+customer_name   = "development"
+revenue_type    = "non-rev"
+requestor_name  = "mahesha"
+
+data_source_name    = ""
+lambda_names        = ["authentication", "authorization", "admin"]
---
+++ terraform/variables.tf
@@ -0,0 +1,87 @@
+variable "aws_region" {
+    description = "AWS region"
+    type = string
+}
+
+variable "stack_name" {
+  description = "The stack name of the project"
+  type = string
+  default = "shavika"
+}
+
+variable "github_owner" {
+    description = "github repository owner"
+    type = string
+}
+
+variable "deploy_env" {
+    description = "deployment environment"
+    type = string
+    default = "dev"
+}
+
+variable "deploy_repo" {
+    description = "deployment github repository"
+    type = string
+}
+
+variable "product" {
+    description = "project name tag"
+    type = string
+    default = "DOMS" 
+}
+
+variable "product_version" {
+  description = "product verison tag"
+  type = string
+  default = "1.0.0"
+}
+
+variable "customer_name" {
+  description = "customer name tag"
+  type = string
+  default = "epd"
+}
+
+variable "revenue_type" {
+    description = "revenue type tag"
+    type = string
+    default = "non-rev" 
+}
+
+variable "requestor_name" {
+    description = "requestor name tag"
+    type = string
+    default = ""
+}
+
+variable "pipeline_token" {
+  description = "Github token passed in from CI workflow"
+  type = string
+  default = ""
+}
+
+variable "function_directory" {
+  description = "Github repo function path"
+  type = string
+  default = "."
+}
+
+## DynamoDb ###################################
+variable "data_source_name" {
+  type        = string
+  description = "DynamoDB service name for data repository with all tenant base."
+}
+
+## lambda ###################################
+variable "lambda_names" {
+  type    = list(string)
+  description = "Using this variables creating aws lambdas, lambda roles and code archive"
+  default = [
+    "authentication",
+    "authorization",
+    "admin"
+  ]
+}
+
+
